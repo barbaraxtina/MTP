5ee098b78696266eea1fe4d8333d8f18f536cd28
barbaraxtina <barbaraxtina@hotmail.com>
2017-09-21 10:33:29 -0300

    fim de aula

---
+++ aula20170905/seq1.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main ()
+
+{
+	float l, a;
+	printf ("Digite a medida do lado do quadrado:	");
+	scanf ("%f", &l); // para salvar o número na variável, usar o "&" e o nome da incógnita; para usar o double "%lf", para usar o inteiro "%d", para usar float "%f".
+	a= l*l;
+	printf ("%.2f", a);
+}
